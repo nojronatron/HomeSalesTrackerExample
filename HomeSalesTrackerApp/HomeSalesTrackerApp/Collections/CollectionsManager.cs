2cac3272459042c1adb4c2511debe22176fa65a0
Jon Rumsey <jon.rumsey@gmail.com>
2020-08-20 16:02:01 -0700

    Added CollectionsManager to start refactoring MainWindow to reduce code-behind. Added tests.

---
+++ HomeSalesTrackerApp/HomeSalesTrackerApp/Collections/CollectionsManager.cs
@@ -0,0 +1,90 @@
+﻿using HSTDataLayer;
+using HSTDataLayer.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeSalesTrackerApp.Helpers
+{
+    public class CollectionsManager
+    {
+        private Logger logger = null;
+        public static RealEstateCosCollection reCosCollection = null;
+        public static HomesCollection homesCollection = null;
+        public static PeopleCollection<Person> peopleCollection = null;
+        public static HomeSalesCollection homeSalesCollection = null;
+
+        public CollectionsManager()
+        {
+            logger = new Logger();
+            reCosCollection = new RealEstateCosCollection();
+            homesCollection = new HomesCollection();
+            peopleCollection = new PeopleCollection<Person>();
+            homeSalesCollection = new HomeSalesCollection();
+            logger.Data("CollectionsManager", "CTOR initialize executed");
+            logger.Flush();
+        }
+
+        public bool InitializeCollections()
+        {
+            bool result = true;
+            try
+            {
+                InitHomesCollection();
+                InitHomeSalesCollection();
+                InitPeopleCollection();
+                InitRealEstateCompaniesCollection();
+            }
+            catch
+            {
+                logger.Data("InitializeCollections", "Failed.");
+                logger.Flush();
+                result = false;
+            }
+            return result;
+        }
+
+        public static void InitRealEstateCompaniesCollection()
+        {
+            reCosCollection = new RealEstateCosCollection();
+            List<RealEstateCompany> recos = EntityLists.GetTreeListOfRECompanies();
+            foreach (var reco in recos)
+            {
+                reCosCollection.Add(reco);
+            }
+        }
+
+        public static void InitHomesCollection()
+        {
+            homesCollection = new HomesCollection();
+            List<Home> homes = EntityLists.GetTreeListOfHomes();
+            foreach (var home in homes)
+            {
+                homesCollection.Add(home);
+            }
+        }
+
+        public static void InitPeopleCollection()
+        {
+            peopleCollection = new PeopleCollection<Person>();
+            List<Person> people = EntityLists.GetListOfPeople();
+            foreach (var person in people)
+            {
+                peopleCollection.Add(person);
+            }
+        }
+
+        public static void InitHomeSalesCollection()
+        {
+            homeSalesCollection = new HomeSalesCollection();
+            List<HomeSale> homeSales = EntityLists.GetListOfHomeSales();
+            foreach (var homeSale in homeSales)
+            {
+                homeSalesCollection.Add(homeSale);
+            }
+        }
+
+    }
+}
