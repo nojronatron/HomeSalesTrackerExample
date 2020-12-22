63baece17dff04c5826f6f4ee855e265f99ec6cb
Jonathan Rumsey <Jonathan.Rumsey@BellevueCollege.edu>
2020-12-22 08:22:25 -0800

    Checkpoint.

--- HomeSalesTrackerApp/HomeSalesTrackerApp/CrudWindows/AddHomeView.xaml.cs
+++ HomeSalesTrackerApp/HomeSalesTrackerApp/CrudWindows/AddHomeView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HomeSalesTrackerApp.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +19,48 @@ namespace HomeSalesTrackerApp.CrudWindows
     /// <summary>
     /// Interaction logic for AddHomeView.xaml
     /// </summary>
-    public partial class AddHomeView : UserControl
+    public partial class AddHomeView : UserControl, IObserver<NotificationData>
     {
+        private string NotificationMessage { get; set; }
         public AddHomeView()
         {
             InitializeComponent();
+        }
+
+        private void RefreshOwnersCombobox()
+        {
+            var existingOwnersList = (from p in ((App)Application.Current)._peopleCollection
+                                      where p.Owner != null
+                                      select p).ToList();
+            OwnersList.ItemsSource = existingOwnersList;
+            OwnersList.SelectedIndex = -1;
+        }
+
+        public void OnCompleted()
+        {
+            ;
+        }
+
+        public void OnError(Exception error)
+        {
+            ;
+        }
+
+        public void OnNext(NotificationData value)
+        {
+            if (value.ChangeCount > 0)
+            {
+                if (value.DataType.Contains("Owner") || value.DataType.Contains("Person"))
+                {
+                    RefreshOwnersCombobox();
+                    NotificationMessage = "Received an update to the Owners Combo Box.";
+                }
+            }
+
+            else
+            {
+                NotificationMessage = "Received a message with no applicable changes.";
+            }
         }
     }
 }
--- HomeSalesTrackerApp/HomeSalesTrackerApp/CrudWindows/AddHomeViewModel.cs
+++ HomeSalesTrackerApp/HomeSalesTrackerApp/CrudWindows/AddHomeViewModel.cs
@@ -1,18 +1,30 @@
-﻿using System;
+﻿using HSTDataLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace HomeSalesTrackerApp.CrudWindows
 {
     class AddHomeViewModel
     {
+        protected Home NewHome { get; set; }
+        protected Person NewPerson { get; set; }
+        protected Owner NewOwner { get; set; }
         public AddHomeViewModel()
         {
-
+            NewHome = new Home();
+            NewPerson = new Person();
+            NewOwner = new Owner();
         }
-
-
+        protected void AddNewHome()
+        {
+            if (NewHome != null)
+            {
+                ((App)Application.Current)._homesCollection.Add(NewHome);
+            }
+        }
     }
 }
