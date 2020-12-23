bcec4b28f58d5e9733974ae7fe150631498464f3
Jon Rumsey <jon.rumsey@gmail.com>
2020-12-22 20:45:17 -0800

    Validation now happening, more to do. Not all UI elements are hooked up yet.

--- HomeSalesTrackerApp/HomeSalesTrackerApp/DisplayModels/HomeDisplayModel.cs
+++ HomeSalesTrackerApp/HomeSalesTrackerApp/DisplayModels/HomeDisplayModel.cs
@@ -1,18 +1,140 @@
-﻿namespace HomeSalesTrackerApp.DisplayModels
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace HomeSalesTrackerApp.DisplayModels
 {
-    public class HomeDisplayModel
+    public class HomeDisplayModel : IDataErrorInfo, INotifyPropertyChanged
     {
         public int HomeID { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string Zip { get; set; }
+        private string _address;
+        public string Address
+        {
+            get
+            {
+                return _address;
+            }
+            set
+            {
+                if (value != _address)
+                {
+                    _address = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string _city;
+        public string City
+        {
+            get
+            {
+                return _city;
+            }
+            set
+            {
+                if (value != _city)
+                {
+                    _city = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string _state;
+        public string State
+        {
+            get
+            {
+                return _state;
+            }
+            set
+            {
+                if (value != _state)
+                {
+                    _state = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string _zip;
+        public string Zip
+        {
+            get
+            {
+                return _zip;
+            }
+            set
+            {
+                if (value != _zip)
+                {
+                    _zip = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
         public string ZipCode
         {
             get
             {
                 return $"{Zip.Substring(0, 5)}-{Zip.Substring(5, 4)}";
             }
+        }
+
+        public string Error => "Error method called.";
+
+        public string this[string columnName]
+        {
+            get
+            {
+                string result = null;
+                switch (columnName)
+                {
+                    case "Address":
+                        {
+                            if (this.Address.Length < 1 || this.Address.Length > 50)
+                            {
+                                result = "Address limited to 50 characters.";
+                            }
+                            break;
+                        }
+                    case "City":
+                        {
+                            if (this.City.Length < 2 || this.City.Length > 30)
+                            {
+                                result = "City limited to 50 characters.";
+                            }
+                            break;
+                        }
+                    case "State":
+                        {
+                            if (this.State.Length != 2)
+                            {
+                                result = "State must be 2 alpha-numeric characters.";
+                            }
+                            break;
+                        }
+                    case "Zip":
+                        {
+                            if (this.Zip.Length < 5 || this.Zip.Length > 9)
+                            {
+                                result = "Zip code must be 5 or 9 digits without spaces.";
+                            }
+                            break;
+                        }
+                    default:
+                        {
+                            result = string.Empty;
+                            break;
+                        }
+                }
+
+                return result;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
 
         public override string ToString()
